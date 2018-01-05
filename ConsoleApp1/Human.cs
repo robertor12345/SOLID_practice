3b7ade85cc91410d1a7a2505f5e836cf50c72c67
Roberto Branco-Rhodes <roberto.branco-rhodes@asos.com>
2018-01-05 09:13:47 +0000

    intitial commit

---
+++ ConsoleApp1/Human.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Animals
+{
+    public class Human : iMammals 
+    {
+        static void Main(string[] args)
+        {
+
+            Human rob = new Human();
+            rob.Breathe();
+            rob.Sleep();
+            rob.Eat();
+
+        }
+
+        public string Breathe()
+        {
+            var breath = "brrrrrrr";
+            return breath;
+        }
+
+        public string Eat()
+        {
+            var food = "dim sum";
+            return food;
+        }
+
+        public string Sleep()
+        {
+            var snoring = "zzzzzz";
+            return snoring;
+        }
+
+      
+    }
+}
---
+++ ConsoleApp1/iMammals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    interface iMammals
+    {
+        String Breathe();
+        String Sleep();
+
+        String Eat();
+        
+
+    }
+}
